d552072d79e4728c17c6b439604c5c7336feda3c
Ru-dr-es-h <rudreshsunagad1999@gmail.com>
2021-08-07 17:15:29 +0530

    submitted by Rudresh

--- elearningproj/src/main/java/com/elr/model/Student.java
+++ elearningproj/src/main/java/com/elr/model/Student.java
@@ -2,84 +2,18 @@ package com.elr.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
-
-public class Student {
-	@Id
-	private int studentId;
-	
-	@Column(name="Student_Name")
-	private String studentName;
-	
-	@Column(name="DOB")
-	private String dob;
-	
-	@OneToOne
-	@JoinColumn(name = "auth_un")
-	private String username;
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity
+@Table(name = "STUDENT")
+public class Student extends User {
 	
 	@OneToMany(mappedBy = "Course")
 	private List enroll;
-	
-	@Override
-	public String toString() {
-		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", dob=" + dob + ", username="
-				+ username + ", enroll=" + enroll + ", auth=" + auth + "]";
-	}
-
-	public Student() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Student(int studentId, String studentName, String dob, String username, List enroll, Auth auth) {
-		super();
-		this.studentId = studentId;
-		this.studentName = studentName;
-		this.dob = dob;
-		this.username = username;
-		this.enroll = enroll;
-		this.auth = auth;
-	}
-
-	public int getStudentId() {
-		return studentId;
-	}
-
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
-
-	public String getStudentName() {
-		return studentName;
-	}
-
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-
-	public String getDob() {
-		return dob;
-	}
-
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
 	public List getEnroll() {
 		return enroll;
@@ -89,17 +23,22 @@ public class Student {
 		this.enroll = enroll;
 	}
 
-	public Auth getAuth() {
-		return auth;
+	@Override
+	public String toString() {
+		return "Student [enroll=" + enroll + "]";
 	}
 
-	public void setAuth(Auth auth) {
-		this.auth = auth;
+	public Student(List enroll) {
+		super();
+		this.enroll = enroll;
 	}
 
-	@Embedded
-	private Auth auth;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
+
 	
 }
