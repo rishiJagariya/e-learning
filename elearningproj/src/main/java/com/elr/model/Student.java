b3dc1ecf450a94c1ba7a0ad803716e68259eb785
Ru-dr-es-h <rudreshsunagad1999@gmail.com>
2021-08-07 17:54:32 +0530

    submitted by rudresh

--- elearningproj/src/main/java/com/elr/model/Student.java
+++ elearningproj/src/main/java/com/elr/model/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Entity
