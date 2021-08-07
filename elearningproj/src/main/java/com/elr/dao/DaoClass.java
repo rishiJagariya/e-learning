32e98690031e4c54267733fd76c46ec2eb2178e1
Rishi jagariya <43576913+rishiJagariya@users.noreply.github.com>
2021-08-07 16:21:46 +0530

    update 4

--- elearningproj/src/main/java/com/elr/dao/DaoClass.java
+++ elearningproj/src/main/java/com/elr/dao/DaoClass.java
@@ -1,5 +1,19 @@
 package com.elr.dao;
 
+import com.elr.model.Auth;
+
 public class DaoClass implements DaoInterface{
+
+	@Override
+	public Auth getAuth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addAuth() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
